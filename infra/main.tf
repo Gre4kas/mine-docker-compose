9c5e3dba09e00bd81e93feb1bcc3301c3075a629
Gre4kas <misaa3818@gmail.com>
2024-07-31 22:38:49 +0300

    Added gateway main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -68,3 +68,28 @@ resource "aws_instance" "minecraft_instance" {
     }
 }
 
+resource "aws_internet_gateway" "minecraft_igw" {
+  vpc_id = aws_vpc.minecraft_vpc.id
+
+  tags = {
+    Name = "minecraft_igw"
+  }
+}
+
+resource "aws_route_table" "minecraft_rt" {
+  vpc_id = aws_vpc.minecraft_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.minecraft_igw.id
+  }
+
+  tags = {
+    Name = "minecraft_rt"
+  }
+}
+
+resource "aws_route_table_association" "minecraft_rta" {
+  subnet_id      = aws_subnet.minecraft_subnet.id
+  route_table_id = aws_route_table.minecraft_rt.id
+}
